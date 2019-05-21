72c10908950c5a47a38a0ae919e6a0ff8813807d
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-05-21 00:12:01 -0300

    Adding end page and save score in storage

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question")
 const choices = Array.from(document.getElementsByClassName("choice-text"))
-const questionCounterText = document.getElementById('questionCounter')
+const progressText = document.getElementById('progressText')
 const scoreText = document.getElementById('score')
+const progressBarFull = document.getElementById('progressBarFull')
 
 let currentQuestion = {}
 let acceptAnswers = false
@@ -10,32 +11,32 @@ let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
-    {
-      question: "Inside which HTML element do we put the JavaScript??",
-      choice1: "<script>",
-      choice2: "<javascript>",
-      choice3: "<js>",
-      choice4: "<scripting>",
-      answer: 1
-    },
-    {
-      question:
-        "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      choice1: "<script href='xxx.js'>",
-      choice2: "<script name='xxx.js'>",
-      choice3: "<script src='xxx.js'>",
-      choice4: "<script file='xxx.js'>",
-      answer: 3
-    },
-    {
-      question: " How do you write 'Hello World' in an alert box?",
-      choice1: "msgBox('Hello World');",
-      choice2: "alertBox('Hello World');",
-      choice3: "msg('Hello World');",
-      choice4: "alert('Hello World');",
-      answer: 4
-    }
-  ];
+{
+question: "Inside which HTML element do we put the JavaScript??",
+choice1: "<script>",
+choice2: "<javascript>",
+choice3: "<js>",
+choice4: "<scripting>",
+answer: 1
+},
+{
+question:
+"What is the correct syntax for referring to an external script called 'xxx.js'?",
+choice1: "<script href='xxx.js'>",
+choice2: "<script name='xxx.js'>",
+choice3: "<script src='xxx.js'>",
+choice4: "<script file='xxx.js'>",
+answer: 3
+},
+{
+question: " How do you write 'Hello World' in an alert box?",
+choice1: "msgBox('Hello World');",
+choice2: "alertBox('Hello World');",
+choice3: "msg('Hello World');",
+choice4: "alert('Hello World');",
+answer: 4
+}
+];
 
 // CONSTANTES
 const CORRECT_BONUS = 10
@@ -50,11 +51,15 @@ startGame = () => {
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
         // go to the end page
         return window.location.assign("/end.html")
     }
     questionCounter++
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`
+
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionIndex]
